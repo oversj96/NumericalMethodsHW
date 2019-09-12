c0177980ddfbad33cbf6da2e563f879f9ea14047
oversj96 <45669103+oversj96@users.noreply.github.com>
2019-09-12 15:32:03 -0500

    finished computer program 2

--- Homework 3/Computer Program 2.py
+++ Homework 3/Computer Program 2.py
@@ -3,21 +3,22 @@
 # Purpose: Use Newton's Method to find A^(1/N).
 # Date: 9-12-2019
 
-def root(A, N):
-    return A**(1/N)
+def root(p, A, N):
+    return p - ((p**N - A)/(N*p))
 
-def iterate(p0, N, func, tol=1e-6, maxiter=None):
-    if p0 is 0:
+def iterate(A, N, func, tol=1e-6, maxiter=None):
+    p0 = A
+    if A is 0:
         return 0
-    if p0 < 0 and N % 2 == 0:
+    if A < 0 and N % 2 == 0:
         print(f"Error: A = {p0} and N = {N} are respectively negative and even.")
         return None
     iter_val = [p0]
     i: int = 0
     e = 1
     while e > tol:
-        p = func(iter_val[i], N)
-        e = abs(iter_val[i] - p) / iter_val[i]
+        p = func(iter_val[i], A, N)
+        e = abs(iter_val[i] - p)
         iter_val.append(p)
         i += 1
         if maxiter is not None and i >= maxiter:
@@ -25,6 +26,6 @@ def iterate(p0, N, func, tol=1e-6, maxiter=None):
     return iter_val
 
 
-print(iterate(1, 4, root))
+print(iterate(2, 4, root))
 print()
 
